e19a5ad2a5ea52cfef78a29a14984e1e1994e45e
Marius <marius.farcas94@gmail.com>
2018-03-17 10:36:27 +0200

    [SETUP] - created project setup, with all the needed dependencies. Also added checkstyle and findbugs.

--- src/main/java/com/acquila/AcquilaApplication.java
+++ src/main/java/com/acquila/AcquilaApplication.java
@@ -1,7 +1,20 @@
 package com.acquila;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+import lombok.extern.log4j.Log4j2;
+
 /**
  * Acquila application main class.
  */
+@Log4j2
+@SpringBootApplication
+@ComponentScan("com.acquila")
 public class AcquilaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AcquilaApplication.class, args);
+    }
 }
