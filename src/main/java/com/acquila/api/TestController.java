1e3449b73336fee4af77262dc270f030ea2e5f5f
Marius <marius.farcas94@gmail.com>
2018-03-22 22:11:19 +0200

    added logging configuration, configured database connection, created init-db script, created test controller

---
+++ src/main/java/com/acquila/api/TestController.java
@@ -0,0 +1,16 @@
+package com.acquila.api;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Used for random testing purposes.
+ */
+@RestController
+public class TestController {
+
+    @GetMapping("/test")
+    public String test() {
+        return "Hello, Acquila user! ";
+    }
+}
